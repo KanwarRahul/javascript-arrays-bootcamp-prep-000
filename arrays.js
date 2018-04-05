7f658e66bf926429aa1e40fefb2b8715d58fae59
singhrahulkanwar@gmail.com Singh <singhrahulkanwar@gmail.com>
2018-04-05 06:48:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,37 @@ function  destructivelyAddElementToBeginningOfArray(chocolateBars,element)
 return test.push(addElementToBeginningOfArray(chocolateBars,element))
 }
 
+function addElementToEndOfArray(chocolateBars,element) 
+{ 
+  var test1 =[]
+  test1.push(element)
+  return test1
+  
+}
+function  destructivelyAddElementToEndOfArray(chocolateBars,element)
+{
+ chocolateBars.push(element) 
+ return chocolateBars
+}
+
+  function accessElementInArray(chocolateBars,index)
+  {
+    return chocolateBars[index]
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray(chocolateBars)
+  {
+    return chocolateBars.shift()
+  } 
+  
+  function  removeElementFromBeginningOfArray(chocolateBars)
+  {
+    return chocolateBars.slice(1)
+  }
+    
+  function destructivelyRemoveElementFromEndOfArray(chocolateBars)
+    {
+      return chocolateBars.pop()
+    }
+    
+    
